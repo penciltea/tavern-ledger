728ce5dbb89175f8b9e508efacf6752c35fe0138
Brooke Yombalakian <penciltea@aol.com>
2025-03-25 11:32:18 -0400

    Adding search functionality

--- components/quest-list.tsx
+++ components/quest-list.tsx
@@ -1,7 +1,8 @@
 'use client'
 import useSWR from "swr";
 import { Suspense, useState } from "react";
-import { Button, List, ListItem, ListItemText, Paper, Typography, Divider, ListSubheader } from "@mui/material";
+import { TextField, Button, List, ListItem, ListItemText, Paper, Typography, Divider, ListSubheader } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 import type { Quest, QuestList } from '@/components/quest.interface';
 import { useRouter } from "next/navigation";
 
@@ -11,32 +12,86 @@ const fetcher = (url: string) => fetch(url, { method: "GET" }).then((res) => res
 
 export default function QuestList(){
     const router = useRouter();
-    const { data: quests, error, mutate } = useSWR(`${API_URL}/api/quests`, fetcher);
-    const [selectedItem, setSelectedItem] = useState<Quest | null>(null);
-    
+    const { data: quests, error } = useSWR(`${API_URL}/api/quests`, fetcher);
+
+    const [searchText, setSearchText] = useState("");
+    const [filtersOpen, setFiltersOpen] = useState(false);
+    const [filteredQuests, setFilteredQuests] = useState(quests);
+
     if(error){ console.log(error); }
 
     if (error) return <Typography>Error loading quests.</Typography>;
     if (!quests) return <Typography>Loading...</Typography>;
 
+    
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const text = e.target.value.toLowerCase();
+        setSearchText(text);
+        setFilteredQuests(
+          quests.filter((quest: Quest) =>
+            quest.questName.toLowerCase().includes(text)
+          )
+        );
+      };
+
     return (
         <Paper sx={{p: 1, height: '100%', maxHeight: '80vh', overflow: 'auto'}}>
             <Suspense fallback={<Typography variant="body2">Loading Quests...</Typography>}>
             <List>
-                <ListSubheader>
-                    <Button variant="contained" color="primary" fullWidth onClick={() => router.push("/quest/")}>
-                        Create New Quest
+                <ListSubheader sx={{p: 1}}>
+                    <TextField
+                        label="Search Quests"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        value={searchText}
+                        onChange={handleSearch}
+                        sx={{ marginBottom: 2 }}
+                    />
+                    <Button startIcon={ <AddIcon /> } variant="contained" color="primary" fullWidth onClick={() => router.push("/quest/")}>
+                        Create
                     </Button>
                 </ListSubheader>
-                {quests.map((quest: Quest, index: number) => (
+                {(filteredQuests || quests).map((quest: Quest, index: number) => (
                     <div key={quest._id} >
                         <ListItem
-                            
                             onClick={() => router.push(`/quest/${quest._id}`)}
+                            sx={{ 
+                                cursor: "pointer"
+                             }}
                         >
                             <ListItemText 
-                            primary={quest.questName} 
-                            secondary={quest.status}
+                            sx={{
+                                display: "flex",
+                                flexFlow: "row",
+                                justifyContent: "space-between"
+                            }}
+                            primary={
+                                <Typography 
+                                    variant="body1" 
+                                    sx={{
+                                        whiteSpace: "nowrap",
+                                        overflow: "hidden",
+                                        textOverflow: "ellipsis",
+                                        mr: 2
+                                    }}
+                                >
+                                    {quest.questName}
+                                </Typography>
+                            }
+                            secondary={
+                                <Typography 
+                                    variant="body2" 
+                                    color="textSecondary"
+                                    sx={{ 
+                                        width: '12rem',
+                                        textAlign: "right"
+                                    }}
+                                >
+                                    Status: {quest.status}
+                                </Typography>
+                            }
                             />
                         </ListItem>
                         {/* hide divider for last item in list */}
